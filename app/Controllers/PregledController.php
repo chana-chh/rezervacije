e9f3455d1e06c3e963c8082d0b86a58728019a7f
kure-kg <stashakg@gmail.com>
2019-12-25 22:11:02 +0100

    Dogadjaji

--- app/Controllers/PregledController.php
+++ app/Controllers/PregledController.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\Sala;
+use App\Models\Termin;
 
 class PregledController extends Controller
 {
@@ -15,6 +16,32 @@ class PregledController extends Controller
 
         $url = $this->router->pathFor('termin.dodavanje.get');
 
-        $this->render($response, 'kalendar.twig', compact('sale', 'url', 'datum'));
+        $modelTermin = new Termin();
+        $termini = $modelTermin->all();
+
+        $naslovi = array();
+        $idijevi = array();
+        $datumi = array();
+        $poceci = array();
+        $krajevi = array();
+        $detalji = array();
+
+        foreach ($termini as $termin) {
+            $naslovi[] = $termin->sala()->naziv;
+            $idijevi[] = $termin->id;
+            $datumi[] = $termin->datum;
+            $poceci[] = $termin->pocetak;
+            $krajevi[] = $termin->kraj;
+            $detalji[] = $termin->opis;
+        }
+
+        $naslovie = json_encode($naslovi);
+        $idijevie = json_encode($idijevi);
+        $datumie = json_encode($datumi);
+        $pocecie = json_encode($poceci);
+        $krajevie = json_encode($krajevi);
+        $detaljie = json_encode($detalji);
+
+        $this->render($response, 'kalendar.twig', compact('sale', 'url', 'datum', 'idijevie', 'naslovie', 'datumie', 'pocecie', 'krajevie', 'detaljie'));
     }
 }
