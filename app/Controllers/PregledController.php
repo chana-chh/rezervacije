7891dc5db15ebf27e161e0405db3d782a9b7a8e8
kure-kg <stashakg@gmail.com>
2019-12-26 18:35:49 +0100

    Kalendar, dizajn

--- app/Controllers/PregledController.php
+++ app/Controllers/PregledController.php
@@ -30,12 +30,27 @@ use App\Models\Termin;
         $detalji = array();
 
         foreach ($termini as $termin) {
+            
+            if ($termin->zauzet == 0) {
+                $ikonica = '<i class="fas fa-calendar-minus fa-2x" style="color: #0275d8"></i>';
+            }
+            else if($termin->zauzet == 1){
+                $ikonica = '<i class="fas fa-calendar-plus fa-2x" style="color: #5cb85c"></i>';
+            }else{
+                $ikonica = '<i class="fas fa-calendar-check fa-2x" style="color: #d9534f"></i>';
+            }
+
             $naslovi[] = [($termin->pocetak ? '<strong style="text-align: center; font-size: 1.4em !important"><center>'. date('H:i', strtotime($termin->pocetak)). '</center></strong><center>' : '').$termin->sala()->naziv.'</center>'];
             $idijevi[] = $termin->id;
             $datumi[] = $termin->datum;
             $poceci[] = $termin->pocetak;
             $krajevi[] = $termin->kraj;
-            $detalji[] = '<strong style="font-size: 1.4em !important">'.$termin->opis.'</strong><br><span><img src=" /rezervacije/pub/img/res.jpg" class="img-fluid" alt="Responsive image"></span>';
+            // Dodaj polje u bazi boja i daj vrednosti 0,1,2 kao id sala i u zavisnosti od toga se menja back dogadjaja i skini komentare u eventRender
+            //$detalji[] = $termin->boja; 
+            // $detalji[] = '<strong style="font-size: 1.4em !important">'.$termin->opis.'</strong><br><span><img src=" /rezervacije/pub/img/res.jpg" class="img-fluid" alt="Responsive image"></span>';
+            //Varijanta sa FontAwesome i ZAUZETO
+            $detalji[] = $ikonica;
+
         }
 
         $naslovie = json_encode($naslovi);
