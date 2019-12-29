2f360ae9121623ced20f3dcba42b064142b3bdad
chana-chh <chanasrb@gmail.com>
2019-12-29 13:47:11 +0100

    validation finished

--- app/Controllers/ValidationController.php
+++ app/Controllers/ValidationController.php
@@ -2,12 +2,9 @@
 
 namespace App\Controllers;
 
-// use App\Models\Termin;
-// use App\Models\Sala;
-
 class ValidationController extends Controller
 {
-        public function getValidation($request, $response)
+    public function getValidation($request, $response)
     {
         $this->render($response, 'validation.twig');
     }
@@ -15,9 +12,22 @@ class ValidationController extends Controller
     public function postValidation($request, $response)
     {
         $data = $request->getParams();
+        unset($data['csrf_name']);
+        unset($data['csrf_value']);
 
-        dd($data);
-        
-        return $response->withRedirect($this->router->pathFor('pocetna', ['datum' => $datum]));
+        $validation_rules = [
+            'fname' => [
+                'minlen' => 4
+            ],
+        ];
+        $this->validator->validate($data, $validation_rules);
+
+        if ($this->validator->hasErrors()) {
+            $this->flash->addMessage('danger', 'Došlo je do greške.');
+            return $response->withRedirect($this->router->pathFor('valid'));
+        } else {
+            $this->flash->addMessage('success', 'Validacija prosla.');
+            return $response->withRedirect($this->router->pathFor('pocetna'));
+        }
     }
 }
